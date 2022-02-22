3e5d09b5da523cc4ae205012686479459c4bfc59
Moose Abdool <moose_abdool@airbnb.com>
2022-02-21 22:30:10 -0500

    add support for reduced types

--- pkmn_constants.py
+++ pkmn_constants.py
@@ -1,10 +1,21 @@
 NUM_PKMN_TYPES = 18
 
-PKMN_TYPES = ['Bug', 'Dark', 'Dragon', 'Electric', 'Fairy','Fighting','Fire','Flying','Ghost','Grass','Ground','Ice','Normal','Poison','Psychic','Rock','Steel','Water']
+PKMN_TYPES = ['Bug', 'Dark', 'Dragon', 'Electric', 'Fairy','Fighting',
+              'Fire','Flying','Ghost','Grass','Ground','Ice','Normal','Poison',
+              'Psychic','Rock','Steel','Water']
+
+REDUCED_PKMN_TYPES = ['Bug', 'Dragon', 'Electric', 'Fighting',
+              'Fire','Ghost','Grass','Normal','Poison',
+              'Psychic','Rock','Steel','Water']
+
 
 assert len(PKMN_TYPES) == 18
 
+CLASS_IDX_2_PKMN_TYPE_REDUCED = dict()
 CLASS_IDX_2_PKMN_TYPE = dict()
 
 for i in range(0, len(PKMN_TYPES)):
   CLASS_IDX_2_PKMN_TYPE[i] = PKMN_TYPES[i]
+
+for i in range(0, len(REDUCED_PKMN_TYPES)):
+  CLASS_IDX_2_PKMN_TYPE_REDUCED[i] = REDUCED_PKMN_TYPES[i]
