f63d9820d4dd5fd02ef3c1be35ad4e9e22c5afe9
Moose Abdool <moose_abdool@airbnb.com>
2022-02-23 20:17:58 -0500

    add whole noise image input

--- utils.py
+++ utils.py
@@ -15,6 +15,17 @@ def get_noise(n_samples, z_dim, device='cpu'):
     '''
     return torch.randn(n_samples, z_dim, device=device)
 
+# method to get the noise (for training time) - we just use a normal distribution
+def get_image_noise(n_samples, image_dim, device='cpu'):
+    '''
+    Function for creating noise vectors: Given the dimensions (n_samples, z_dim)
+    creates a tensor of that shape filled with random numbers from the normal distribution.
+    Parameters:
+      n_samples: the number of samples to generate, a scalar
+      z_dim: the dimension of the noise vector, a scalar
+      device: the device type
+    '''
+    return torch.randn((n_samples, 1, image_dim, image_dim), device=device)
 
 # todo: add method to get conditional sample from dataset
 
